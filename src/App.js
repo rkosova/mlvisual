cc3825240500171c3767a015366d5a770924afac
Ron Kosova <ron.kosova@gmail.com>
2022-12-10 20:39:25 +0200

    Classification function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@
 // import './App.css';
 
 class DataPoint {
-  constructor (x ,y) {
+  #x;
+  #y;
+  #classValue;
+
+  constructor (x, y, classValue) {
     this.x = x;
     this.y = y;
+    this.classValue = classValue;
+  }
+
+  setClass = (classValue) => {
+    this.classValue = classValue;
+  }
+
+  getClass = () => {
+    return this.classValue;
   }
 }
 
@@ -12,7 +25,7 @@ class DataPoint {
  * Gets two dimensional distance between two initialized DataPoint objects.
  */
 const getDistance = (a, b) => {
-  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y)**2)
+  return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y)**2);
 }
 
 const getKNeighbours = (unclassifiedPoint, classifiedPoints, k) => {
@@ -20,36 +33,69 @@ const getKNeighbours = (unclassifiedPoint, classifiedPoints, k) => {
   /**
    * Issues
    * 
-   * Will overwrite points with same distance. Possible solution is to split into two arrays
+   * Will overwrite points with same distance. Possible solution is to split into two arrays or key value pair reversed
    */
-  var distancePointPair = new Map()
+  var distancePointPair = new Map();
 
   for (let i = 0; i < classifiedPoints.length; i++) {
-    distancePointPair.set(getDistance(unclassifiedPoint, classifiedPoints[i]), classifiedPoints[i]) 
+    distancePointPair.set(getDistance(unclassifiedPoint, classifiedPoints[i]), classifiedPoints[i]);
   }
 
-  var distances = Array.from(distancePointPair.keys())
+  var distances = Array.from(distancePointPair.keys());
 
   for (let i = 0; i < distances.length - 1; i++) {
-    let current = distances[i]
+    let current = distances[i];
     for (let j = i + 1; j < distances.length; j++) {
       if (current > distances[j]) {
-        let temp = distances[j]
-        distances[j] = current
-        distances[i] = temp
-        break
+        let temp = distances[j];
+        distances[j] = current;
+        distances[i] = temp;
+        break;
       }
     }
   }
 
-  var KNeighbours = []
+  var KNeighbours = [];
 
   for (let i = 0; i < k; i++) {
-    KNeighbours[i] = distancePointPair.get(distances[i])
+    KNeighbours[i] = distancePointPair.get(distances[i]);
   }
 
   return KNeighbours;
   
+}
+
+
+const classify = (KNeighbours) => {
+  var classes = [];
+
+  for (let i = 0; i < KNeighbours.length; i++) {
+    classes[i] = KNeighbours[i].getClass();
+  }
+
+  var uniqueClasses = [...new Set(classes)] // not too demanding considering k is usually a small value. may be useless
+
+  var uClassesCount = new Map();
+  uClassesCount.set(classes[0], 1);
+
+  for (let i = 1; i < classes.length; i++) {
+    if (!uClassesCount.has(classes[i])) {
+      uClassesCount.set(classes[i], 1);
+    } else {
+      uClassesCount.set(classes[i], uClassesCount.get(classes[i]) + 1);
+    }
+  } 
+
+  var currentMostRepeatingClass = uniqueClasses[0];
+
+  for (let i = 1; i < uniqueClasses.length; i++) {
+    if (uClassesCount.get(currentMostRepeatingClass) < uClassesCount.get[uniqueClasses[i]]) {
+      currentMostRepeatingClass = uniqueClasses[i];
+    }
+  }
+
+
+  return currentMostRepeatingClass;
 }
 
 // const userInput = () => {
@@ -57,8 +103,13 @@ const getKNeighbours = (unclassifiedPoint, classifiedPoints, k) => {
 // }
 
 const App = () => {
-  var KNeighbours = getKNeighbours(new DataPoint(0, 0), [new DataPoint(1, 4), new DataPoint(302, 343), new DataPoint(1, 1), new DataPoint(3, 5)], 3)
-  console.log(KNeighbours[2].x, KNeighbours[2].y)
+  var KNeighbours = getKNeighbours(new DataPoint(0, 0, null), 
+                                   [new DataPoint(1, 4, "A"), new DataPoint(302, 343, "C"), new DataPoint(1, 1, "A"), new DataPoint(3, 5, "B")],
+                                   3);
+
+
+  console.log(classify(KNeighbours))
+  
   return (
     <>
     </>
